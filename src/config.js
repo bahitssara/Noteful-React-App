70ab9bb98d9af3f0084ddbc6f730f79de16ac1de
Sara Mayberry <saraamayberry@yahoo.com>
2019-05-23 21:55:36 -0600

    deploy2

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000`,
-    API_KEY: '9e0502db-a2e5-407c-92cc-efc828903c9d',
+    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_KEY: process.env.API_KEY,
   }
